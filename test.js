2787c88fd57d25e711636251fff7e15ee4bab1d0
olaiyaeverjoy <olaiyaeverjoy02@gmail.com>
2026-01-07 20:24:46 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,24 @@
+// let firstName = 'Olaiya';
+// let lastName = 'Everjoy';
+// console.log(firstName);
+
+// let fullName = firstName + ' ' + lastName;
+
+// console.log(fullName);
+
+// console.log(fullName.toUpperCase());
+// let result = fullName.toLowerCase();
+// console.log(result); 
+
+
+
+// const names = [ 'femi', 'everjoy', 'olaiya' ];
+
+// for(let i = 0; i < names.length; i++){
+//     console.log(names[i]);
+// }
+
+const password = [password];{
+if (password >=5)
+    console.log('password is strong');
+}
