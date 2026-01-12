a0a8e800ae2d391ffb7f8c8be56790f6fcb80db0
olaiyaeverjoy <olaiyaeverjoy02@gmail.com>
2026-01-12 16:37:37 +0100

    second commit

--- test.js
+++ test.js
@@ -11,14 +11,71 @@
 // console.log(result); 
 
 
-
 // const names = [ 'femi', 'everjoy', 'olaiya' ];
 
 // for(let i = 0; i < names.length; i++){
-//     console.log(names[i]);
+// //     console.log(names[i]);
+// let html = `<div>${names[i]}</div>`
+// console.log(html);
 // }
 
-const password = [password];{
-if (password >=5)
-    console.log('password is strong');
+// const password = 'password';
+// if(password.length >= 12){
+//     console.log('that password is super strong');
+// } else if (password.length <=8){
+//     console.log('password is not strong enough');
+// }
+
+// let i  = 0;
+// while(i <= 5){
+//     console.log('loop is ', i);
+// i++;    
+// }
+// const numbers = [ 10, 20, 0, 30, 40, 50 ];
+
+
+// for(let i = 0; i < numbers.length; i++){
+//     if (numbers[i] === 0)
+//         continue;
+    
+//     console.log('The numbers are: ',numbers[i]);
+    
+//     if (numbers[i] === 40){
+//         console.log('congrats on the highest score')
+//         break; 
+//     }
+    
+// }
+
+// const grade = 'B';
+
+// switch(grade){
+    
+//     case 'A':
+//         console.log('you got a grade A');
+//         break;
+//     case 'B': 
+//         console.log('you got a grade B');       
+//         break;
+//     case 'C':
+//         console.log('you got a grade C');
+//         break;
+//     case 'D':
+//         console.log('you got a grade D');
+//         break;       
+//     case 'E':
+//         console.log('you got a grade E');
+//         break;       
+//     default:
+//         console.log('not a valid grade');
+
+// }
+
+let age = '40'
+
+if(true){
+    let age = '20';
+    console.log('This is the perfect age ',age);
 }
+
+console.log('This is the perfect age ',age);
